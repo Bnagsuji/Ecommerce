9bdf8ff16e8d2efd3c2652a4ef70706808fb79f4
bangsuji <sjbang@lgupluspartners.co.kr>
2025-07-18 07:45:06 +0900

    [config] 도커,스웨거, 롬복 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
+	//swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	//lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
