65c0d9a3c55cb816002361f97cc4023a0a517be0
bangsuji <sjbang@lgupluspartners.co.kr>
2025-08-01 02:56:59 +0900

    [feat] Account 관련 Infra레이어 추가

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/repository/account/AccountRepositoryImpl.java
@@ -0,0 +1,21 @@
+package kr.hhplus.be.server.infrastructure.repository.account;
+
+import kr.hhplus.be.server.domain.account.Account;
+import kr.hhplus.be.server.domain.account.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class AccountRepositoryImpl implements AccountRepository {
+
+    private final AccountJpaRepository accountJpaRepository;
+
+    @Override
+    public Optional<Account> findByUserId(Long userId) {
+        return accountJpaRepository.findByUserId(userId);
+    }
+
+}
