94bd89dca720dfdb63ec405d8be8e003ab9f4f54
bangsuji <sjbang@lgupluspartners.co.kr>
2025-08-01 03:00:31 +0900

    [feat] Order 관련 Infra레이어 추가

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/repository/order/OrderJpaRepository.java
@@ -0,0 +1,8 @@
+package kr.hhplus.be.server.infrastructure.repository.order;
+
+import kr.hhplus.be.server.domain.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+}
