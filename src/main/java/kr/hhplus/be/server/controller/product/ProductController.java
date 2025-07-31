2c215da6428ec4ce912e389e977cf49698bdf41f
bangsuji <sjbang@lgupluspartners.co.kr>
2025-08-01 02:56:28 +0900

    [feat] Product 관련 Infra레이어 추가

--- src/main/java/kr/hhplus/be/server/controller/product/ProductController.java
+++ src/main/java/kr/hhplus/be/server/controller/product/ProductController.java
@@ -4,15 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.controller.product.request.ProductRequest;
 import kr.hhplus.be.server.controller.product.response.ProductResponse;
+import kr.hhplus.be.server.domain.product.Product;
 import kr.hhplus.be.server.service.product.ProductService;
 import kr.hhplus.be.server.service.product.impl.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,12 +22,12 @@ import java.util.List;
 public class ProductController {
 
 
-    private final ProductService ProductService;
+    private final ProductService productService;
 
     @GetMapping("/{id}")
     @Operation(summary = "상품 상세 조회",description = "상품별 상세 정보 조회하는 API")
     public ResponseEntity<ProductResponse> getProductDetail(@PathVariable("id") Long id) {
-        ProductResponse result = ProductService.getProductById(id);
+        ProductResponse result = productService.getProductById(id);
         return ResponseEntity.ok(result);
     }
 
@@ -37,7 +35,7 @@ public class ProductController {
     @GetMapping("/top-selling")
     @Operation(summary = "3일간 판매량 순 상위 상품 조회", description = "최근 3일간 많이 팔린 순으로 상품 조회하는 API")
     public ResponseEntity<List<ProductResponse>> getTopSellingProducts() {
-        List<ProductResponse> topSellingProducts = ProductService.getTopSellingProducts();
+        List<ProductResponse> topSellingProducts = productService.getTopSellingProducts();
         return ResponseEntity.ok(topSellingProducts);
     }
 
--- src/main/java/kr/hhplus/be/server/repository/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.repository.product;
-
-import kr.hhplus.be.server.domain.product.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
