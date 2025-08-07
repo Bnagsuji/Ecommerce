66748a584ea135e83a2aa6e12dd873ceaecacc06
bangsuji <sjbang@lgupluspartners.co.kr>
2025-08-08 01:14:30 +0900

    [fix] 재고 동시성 관련 코드 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/repository/product/ProductJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/repository/product/ProductJpaRepository.java
@@ -1,10 +1,18 @@
 package kr.hhplus.be.server.infrastructure.repository.product;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Optional<Product> findByIdForUpdate(@Param("id") Long id);
 
 }
