65e57baeb468da27fbcd957f62da866e5ff6ea7f
bangsuji <sjbang@lgupluspartners.co.kr>
2025-08-22 01:33:10 +0900

    [config] 레디스 관련 설정 추가/변경

--- src/main/java/kr/hhplus/be/server/config/redis/RedisConfig.java
+++ src/main/java/kr/hhplus/be/server/config/redis/RedisConfig.java
@@ -4,14 +4,27 @@ import org.redisson.api.RedissonClient;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+
 
     @Bean
     public RedisConnectionFactory connectionFactory(RedissonClient redissonClient) {
         return new RedissonConnectionFactory(redissonClient);
     }
 
+    @Bean
+    public DefaultRedisScript<Long> issueCouponScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource("lua/fcfs_issue.lua"));
+        script.setResultType(Long.class);
+        return script;
+    }
 }
