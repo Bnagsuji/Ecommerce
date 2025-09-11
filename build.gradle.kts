d317b57fae70727972e8c34d0903ccf48bccd1a0
bangsuji <sjbang@lgupluspartners.co.kr>
2025-09-12 01:15:38 +0900

    [feat] influxdb/grafana 환경 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,20 +53,19 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	// Spring Boot 3.3.2와 호환되는 Redisson 버전을 명시 (이 예시에서는 최신 안정 버전을 사용)
 	implementation("org.redisson:redisson-spring-boot-starter:3.28.0")
-
 	//래디스 캐시
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	// https://mvnrepository.com/artifact/org.mapstruct/mapstruct-processor
 	implementation("org.mapstruct:mapstruct-processor:1.4.2.Final")
 	implementation ("org.mapstruct:mapstruct:1.4.2.Final")
-
+	// JSON 직렬화용
+	implementation("com.fasterxml.jackson.core:jackson-databind")
 	//카프카
 	// https://mvnrepository.com/artifact/org.springframework.kafka/spring-kafka
 	implementation("org.springframework.kafka:spring-kafka:3.3.4")
 	// https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
 	implementation("org.apache.kafka:kafka-clients:3.9.1")
-
 	//swagger
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	//lombok
@@ -74,6 +73,9 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
+	//grafana(prometheus)
+	runtimeOnly ("io.micrometer:micrometer-registry-prometheus")
+
 
 	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
 	annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
@@ -83,7 +85,6 @@ dependencies {
 	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	// testcontainers 버전을 최신 안정 버전으로 변경
 	testImplementation("org.testcontainers:testcontainers:1.19.8")
 	testImplementation("org.testcontainers:junit-jupiter:$tc")
 	testImplementation("org.testcontainers:mysql")
